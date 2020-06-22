72b265c18afa0b7ff00e9ac29824e57146b0cd56
Adrian Brooks <adrian@adrianbrooks.com>
2020-06-22 18:52:26 -0400

    Clean scaffold command

--- commands/create-project.js
+++ commands/create-project.js
@@ -5,15 +5,6 @@ if (!args.length) {
   console.error('scaffold needs a <project-name> argument')
   process.exit(1)
 }
-// const process = spawn(
-//   `yarn create react-app packages/${args[0]} --template file:../cra-template-swordsmith`
-// )
-// process.
-// process.stdout.on('data', data => {
-//   // parse data here...
-// })
-
-console.log('path--->', process.env.PATH)
 
 const run = (cmd, args) => {
   const spawn = require('child_process').spawn
